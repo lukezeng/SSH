c0419d636740bcaef4b3dd9b85fe43cab2f370e7
Luke Zeng <lukezeng@live.com>
2015-08-29 14:54:19 +0800

    add spring test and upddate junit version

---
+++ src/main/java/me/gacl/service/UserServiceI.java
@@ -0,0 +1,14 @@
+package me.gacl.service;
+
+/**
+ 4  * ≤‚ ‘
+ 5  * @author gacl
+ 6  *
+ 7  */
+public interface UserServiceI {
+    /**
+     * 11      * ≤‚ ‘∑Ω∑®
+     * 12
+     */
+    void test();
+}
