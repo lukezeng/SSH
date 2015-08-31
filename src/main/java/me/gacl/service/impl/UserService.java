df49bdc8ba32cf5515b063d8d96f2514457087f2
luke <lukezeng@live.com>
2015-08-31 12:50:08 +0800

    added hibernate into pom.xml and utilized log4j for testing logging

--- src/main/java/me/gacl/service/impl/UserService.java
+++ src/main/java/me/gacl/service/impl/UserService.java
@@ -1,13 +1,16 @@
 package me.gacl.service.impl;
 
 import me.gacl.service.IUserService;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service("userService")
 public class UserService implements IUserService {
 
+    private static final Logger logger = Logger.getLogger(UserService.class);
+
     public void test() {
-        System.out.println("Hello World!");
+        logger.info("Hello World!");
     }
 
 }
